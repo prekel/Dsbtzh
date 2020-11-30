e17331e7cb1f72f9eee7b5791f1d0e2594b5afb0
Vladislav Prekel <misterptits@yandex.ru>
2020-12-01 02:14:59 +0700

    try async

--- Dsbtzh/Program.fs
+++ Dsbtzh/Program.fs
@@ -4,11 +4,21 @@ open Discord
 
 open Discord.WebSocket
 
-let from whom =
-    sprintf "from %s" whom
+let from whom = sprintf "from %s" whom
+
+let cl (client: DiscordSocketClient) =
+    async {
+        let! loginAsync =
+            client.LoginAsync(TokenType.Bot, "")
+            |> Async.AwaitTask
+
+        0 |> ignore
+    }
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" 
+    let message = from "F#"
     printfn "Hello world %s" message
-    0 
+    let client = new DiscordSocketClient()
+    cl client |> ignore
+    0
