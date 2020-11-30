814cea708759909213620d7f40890792c24a1453
Vladislav Prekel <misterptits@yandex.ru>
2020-12-01 02:07:28 +0700

    Proj

---
+++ Dsbtzh/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+open Discord
+
+open Discord.WebSocket
+
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" 
+    printfn "Hello world %s" message
+    0 
